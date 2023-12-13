aded3c44122d500b61c6fdc861df9cbe1eda2abb
Rubén Valls <r.vallsaparici@gmail.com>
2023-12-13 10:49:29 +0100

    main project

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyBdXbfoYVTjpsEbpfpVxpGyEp9ZNBFfrA8",
+        authDomain: "freelearn-298ff.firebaseapp.com",
+        projectId: "freelearn-298ff",
+        storageBucket: "freelearn-298ff.appspot.com",
+        messagingSenderId: "129998216885",
+        appId: "1:129998216885:web:6cad947266cf0a79e668fc"
+    }
+};
