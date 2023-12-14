be18e0325015d45898c838e3f2b2e91e1f36d64e
Rubén Valls <r.vallsaparici@gmail.com>
2023-12-14 10:33:29 +0100

    added login and signup components to loginpage

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginRoutingModule } from './login-routing.module';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
 
 @NgModule({
   declarations: [
-    LoginPageComponent
+    LoginPageComponent,
+    LoginFormComponent,
+    SignupFormComponent
   ],
   imports: [
     CommonModule,
