d36e71eb017318746b3716e32b0fa94919701d60
Rubén Valls <r.vallsaparici@gmail.com>
2023-12-18 14:00:34 +0100

    added admins dashboard and routing

--- src/app/admins/admins-routing.module.ts
+++ src/app/admins/admins-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminsDashboardComponent } from './admins-dashboard/admins-dashboard.component';
+import { UsersComponent } from './admins-dashboard/pages/users/users.component';
+import { TechnologiesComponent } from './admins-dashboard/pages/technologies/technologies.component';
+import { CoursesComponent } from './admins-dashboard/pages/courses/courses.component';
+import { InstructorsComponent } from './admins-dashboard/pages/instructors/instructors.component';
 
 const routes: Routes = [
-    { path: '', component: AdminsDashboardComponent },
+    { path: '', component: AdminsDashboardComponent, children: [
+      { path: 'users', component: UsersComponent },
+      { path: 'technologies', component: TechnologiesComponent },
+      { path: 'courses', component: CoursesComponent },
+      { path: 'instructors', component: InstructorsComponent },
+    ] },
 ];
 
 @NgModule({
--- src/app/admins/admins.module.ts
+++ src/app/admins/admins.module.ts
@@ -2,16 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminsDashboardComponent } from './admins-dashboard/admins-dashboard.component';
 import { AdminsRoutingModule } from './admins-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { UsersComponent } from './admins-dashboard/pages/users/users.component';
+import { TechnologiesComponent } from './admins-dashboard/pages/technologies/technologies.component';
+import { CoursesComponent } from './admins-dashboard/pages/courses/courses.component';
+import { InstructorsComponent } from './admins-dashboard/pages/instructors/instructors.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminsDashboardComponent
+    AdminsDashboardComponent,
+    HeaderComponent,
+    UsersComponent,
+    TechnologiesComponent,
+    CoursesComponent,
+    InstructorsComponent
   ],
   imports: [
     CommonModule,
     AdminsRoutingModule,
+    MatButtonModule,
+    MatMenuModule,
   ]
 })
 export class AdminsModule { }
