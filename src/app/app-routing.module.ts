014206491dff97da6edd01be7fbd6b49169269d5
amsiceza <ismacerverazamora@gmail.com>
2023-12-19 13:18:04 +0100

    Create main-page component and module configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule)},
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'students', loadChildren: () => import('./students/students.module').then(m => m.StudentsModule) },
   { path: 'admin', loadChildren: () => import('./admins/admins.module').then(m => m.AdminsModule) },
