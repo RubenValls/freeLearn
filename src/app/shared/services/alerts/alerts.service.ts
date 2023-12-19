23b2c203d2dfda681441c81a86ed16f8f588e8f4
amsiceza <ismacerverazamora@gmail.com>
2023-12-19 12:52:02 +0100

    Implements diferent alert services

--- src/app/shared/services/alerts/alerts.service.ts
+++ src/app/shared/services/alerts/alerts.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertsService {
 
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar) { }
+
+  errorMessage = (message: string, action?: string, duration?: number) => {
+    this._snackBar.open(message, action ? action : "Close", {duration: duration ? duration : 2000, panelClass: 'error-snackbar'})
+  }
+
+  successMessage = (message: string, action?: string, duration?: number) => {
+    this._snackBar.open(message, action ? action : "Close", {duration: duration ? duration : 2000, panelClass: 'success-snackbar'})
+  }
+
+  warningMessage = (message: string, action?: string, duration?: number) => {
+    this._snackBar.open(message, action ? action : "Close", {duration: duration ? duration : 2000, panelClass: 'warning-snackbar'})
+  }
+
 }
