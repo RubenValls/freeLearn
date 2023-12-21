b2d50982ee647b62c2db44bb1bedc89b217eda86
Rubén Valls <r.vallsaparici@gmail.com>
2023-12-21 16:33:26 +0100

    add test to first section main component

--- src/app/main-page/components/first-section-main/first-section-main.component.spec.ts
+++ src/app/main-page/components/first-section-main/first-section-main.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FirstSectionMainComponent } from './first-section-main.component';
+import { BtnBlackComponent } from 'src/app/shared/components/btn-black/btn-black.component';
 
 describe('FirstSectionMainComponent', () => {
   let component: FirstSectionMainComponent;
@@ -8,8 +8,9 @@ describe('FirstSectionMainComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FirstSectionMainComponent]
+      declarations: [FirstSectionMainComponent, BtnBlackComponent]
     });
+
     fixture = TestBed.createComponent(FirstSectionMainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
