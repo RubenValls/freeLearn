563396cba7bda2da9cd2ca866ddab64767d69c5b
Rubén Valls <r.vallsaparici@gmail.com>
2023-12-22 08:46:01 +0100

    fix tech service basic test

--- src/app/admins/admins-dashboard/pages/technologies/service/tech.service.spec.ts
+++ src/app/admins/admins-dashboard/pages/technologies/service/tech.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TechService } from './tech.service';
+import { AdminsModule } from 'src/app/admins/admins.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 describe('TechService', () => {
   let service: TechService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AdminsModule,
+        provideFirestore(() => getFirestore()),
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAuth(() => getAuth()),
+      ]
+    });
     service = TestBed.inject(TechService);
   });
 
