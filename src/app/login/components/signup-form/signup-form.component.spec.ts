e0f9f1df0fb8a5c63d768d244109d6dd39de1a6d
Rubén Valls <r.vallsaparici@gmail.com>
2023-12-22 07:53:18 +0100

    fix signupformcomponent test

--- src/app/login/components/signup-form/signup-form.component.spec.ts
+++ src/app/login/components/signup-form/signup-form.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupFormComponent } from './signup-form.component';
+import { LoginModule } from '../../login.module';
+import { provideMockStore } from '@ngrx/store/testing';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('SignupFormComponent', () => {
+fdescribe('SignupFormComponent', () => {
   let component: SignupFormComponent;
   let fixture: ComponentFixture<SignupFormComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SignupFormComponent]
+      declarations: [SignupFormComponent],
+      imports: [
+        LoginModule,
+        MatSnackBarModule,
+        BrowserAnimationsModule,
+        provideFirestore(() => getFirestore()),
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAuth(() => getAuth()),
+      ],
+      providers: [ provideMockStore({}) ]
     });
     fixture = TestBed.createComponent(SignupFormComponent);
     component = fixture.componentInstance;
