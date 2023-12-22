a9f8a756ea4f43f1fc2987bfa7acfd7cc0c36c32
Rubén Valls <r.vallsaparici@gmail.com>
2023-12-22 08:34:10 +0100

    fix login form component test

--- src/app/login/components/login-form/login-form.component.spec.ts
+++ src/app/login/components/login-form/login-form.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginFormComponent } from './login-form.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModule } from '../../login.module';
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
@@ -8,7 +16,16 @@ describe('LoginFormComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginFormComponent]
+      declarations: [LoginFormComponent],
+      imports: [
+        LoginModule,
+        MatSnackBarModule,
+        BrowserAnimationsModule,
+        provideFirestore(() => getFirestore()),
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAuth(() => getAuth()),
+      ],
+      providers: [ provideMockStore({}) ]
     });
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
