775074e7359539668b66e1585bec596ee29e4dea
Rubén Valls <r.vallsaparici@gmail.com>
2023-12-27 08:03:09 +0100

    Merge branch 'dev-shared' into dev-students

--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -2,23 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentsDashboardComponent } from './students-dashboard/students-dashboard.component';
 import { StudentsRoutingModule } from './students-routing.module';
-import { share } from 'rxjs';
 import { SharedModule } from '../shared/shared.module';
-<<<<<<< HEAD
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-=======
 import { MatDialogModule } from '@angular/material/dialog';
->>>>>>> dev-shared
 
 
 
 @NgModule({
   declarations: [
     StudentsDashboardComponent,
-<<<<<<< HEAD
     SidebarComponent
-=======
->>>>>>> dev-shared
   ],
   imports: [
     CommonModule,
