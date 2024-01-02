88dca00bfeb184c348024914ca6f2813437b2a19
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-02 12:53:56 +0100

    fixed users component tests

--- src/app/admins/admins-dashboard/pages/users/users.component.spec.ts
+++ src/app/admins/admins-dashboard/pages/users/users.component.spec.ts
@@ -1,15 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 import { UsersComponent } from './users.component';
+import { AlertsService } from 'src/app/shared/services/alerts/alerts.service';
+import { UsersService } from 'src/app/shared/services/users/users.service';
+import { AdminsModule } from 'src/app/admins/admins.module';
+import { User } from 'src/app/login/types/user';
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
+  let storeMock: any;
+  let alertMessagesMock: any;
+  let userServiceMock: any;
+
+  beforeEach(async () => {
+    const usersMock: User[] = [
+      {
+        id: '1',
+        displayName: 'John Doe',
+        email: 'john.doe@example.com',
+        phoneNumber: '+1234567890',
+        photoURL: 'http://example.com/john_doe.jpg',
+        providerId: 'provider123',
+        rememberMe: true,
+        role: 'admin',
+        favorites: ['item1', 'item2'],
+        uid: 'user123',
+        authUid: 'auth123'
+      },
+    ];
+  
+    storeMock = {
+      select: jasmine.createSpy('select').and.returnValue(of(usersMock)),
+    };
+
+    alertMessagesMock = {
+      successMessage: jasmine.createSpy('successMessage'),
+      errorMessage: jasmine.createSpy('errorMessage')
+    };
+
+    userServiceMock = {
+      updateUser: jasmine.createSpy('updateUser').and.returnValue(Promise.resolve())
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [UsersComponent],
+      providers: [
+        { provide: Store, useValue: storeMock },
+        { provide: AlertsService, useValue: alertMessagesMock },
+        { provide: UsersService, useValue: userServiceMock }
+      ],
+      imports: [AdminsModule]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UsersComponent]
-    });
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +63,43 @@ describe('UsersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should edit user', async () => {
+    const user = {
+      id: '1',
+      displayName: 'John Doe',
+      email: 'john.doe@example.com',
+      phoneNumber: '+1234567890',
+      photoURL: 'http://example.com/john_doe.jpg',
+      providerId: 'provider123',
+      rememberMe: true,
+      role: 'admin',
+      favorites: ['item1', 'item2'],
+      uid: 'user123',
+      authUid: 'auth123'
+    };
+    userServiceMock.updateUser.and.returnValue(Promise.resolve());
+    await component.onEdit(user);
+    expect(userServiceMock.updateUser).toHaveBeenCalledWith(user.id, user);
+    expect(alertMessagesMock.successMessage).toHaveBeenCalledWith('User update successfully');
+  });
+
+  it('should handle modal', () => {
+    const user = {
+      id: '1',
+      displayName: 'John Doe',
+      email: 'john.doe@example.com',
+      phoneNumber: '+1234567890',
+      photoURL: 'http://example.com/john_doe.jpg',
+      providerId: 'provider123',
+      rememberMe: true,
+      role: 'admin',
+      favorites: ['item1', 'item2'],
+      uid: 'user123',
+      authUid: 'auth123'
+    };
+    const result = component.onModals(user);
+    expect(result).toEqual(user);
   });
 });
--- src/app/shared/components/modals/detail-modal/detail-modal.component.spec.ts
+++ src/app/shared/components/modals/detail-modal/detail-modal.component.spec.ts
@@ -1,15 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 import { DetailModalComponent } from './detail-modal.component';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('DetailModalComponent', () => {
   let component: DetailModalComponent;
   let fixture: ComponentFixture<DetailModalComponent>;
+  let storeMock: any;
+
+  beforeEach(async () => {
+    storeMock = {
+      select: jasmine.createSpy('select').and.returnValue(of({})),
+      dispatch: jasmine.createSpy('dispatch')
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [DetailModalComponent],
+      imports: [
+        MatDialogModule, 
+        MatIconModule
+      ],
+      providers: [
+        FormBuilder,
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {
+          title: '',
+          totalCourses: null,
+          rows: [],
+          form: {},
+          socialMediaForm: {},
+          techsForm: {},
+          techs: [{}],
+          instructors: [],
+          lessons: [],
+          totalLessons: '',
+          isCourse: false,
+          course$: {},
+          course: {}
+        } },
+        { provide: Store, useValue: storeMock }
+      ]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [DetailModalComponent]
-    });
     fixture = TestBed.createComponent(DetailModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +54,11 @@ describe('DetailModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize', () => {
+    component.ngOnInit();
+    expect(storeMock.select).toHaveBeenCalled();
+    expect(storeMock.dispatch).toHaveBeenCalled();
   });
 });
