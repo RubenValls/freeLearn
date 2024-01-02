a53f8386ce69ef50a3721356c78b0e3511c07627
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-02 16:32:27 +0100

    fix admins dashboard tests

--- src/app/admins/admins-dashboard/admins-dashboard.component.spec.ts
+++ src/app/admins/admins-dashboard/admins-dashboard.component.spec.ts
@@ -3,12 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminsDashboardComponent } from './admins-dashboard.component';
 import { AdminsModule } from '../admins.module';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
-fdescribe('AdminsDashboardComponent', () => {
+describe('AdminsDashboardComponent', () => {
   let component: AdminsDashboardComponent;
   let fixture: ComponentFixture<AdminsDashboardComponent>;
-  let store: MockStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
