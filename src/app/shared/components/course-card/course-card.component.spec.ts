4c04859276d443b5ff38a1b7b4881d32044377ff
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-03 11:39:32 +0100

    fix course card component tests

--- src/app/shared/components/course-card/course-card.component.spec.ts
+++ src/app/shared/components/course-card/course-card.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseCardComponent } from './course-card.component';
+import { SharedModule } from '../../shared.module';
 
 describe('CourseCardComponent', () => {
   let component: CourseCardComponent;
@@ -8,7 +9,10 @@ describe('CourseCardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CourseCardComponent]
+      declarations: [CourseCardComponent],
+      imports: [
+        SharedModule
+      ]
     });
     fixture = TestBed.createComponent(CourseCardComponent);
     component = fixture.componentInstance;
@@ -17,5 +21,13 @@ describe('CourseCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should handle description correctly', () => {
+    const shortDescription = 'short description';
+    const longDescription = 'a'.repeat(101);
+
+    expect(component.handleDescription(shortDescription)).toEqual(shortDescription);
+    expect(component.handleDescription(longDescription)).toEqual('a'.repeat(100) + '...');
   });
 });
