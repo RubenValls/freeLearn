df957d6fcfdab56d7a6c840cb45421d540ec81d1
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-03 14:22:34 +0100

    fix sub modal create tests

--- src/app/shared/components/modals/sub-modals/sub-modal-create/sub-modal-create.component.spec.ts
+++ src/app/shared/components/modals/sub-modals/sub-modal-create/sub-modal-create.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubModalCreateComponent } from './sub-modal-create.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SubModalCreateComponent', () => {
   let component: SubModalCreateComponent;
   let fixture: ComponentFixture<SubModalCreateComponent>;
+  let mockDialogRef: jasmine.SpyObj<MatDialogRef<any>>;
 
   beforeEach(() => {
+    mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
     TestBed.configureTestingModule({
-      declarations: [SubModalCreateComponent]
+      declarations: [SubModalCreateComponent],
+      imports: [
+        SharedModule,
+        MatDialogModule,
+        BrowserAnimationsModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: mockDialogRef }
+      ]
     });
     fixture = TestBed.createComponent(SubModalCreateComponent);
     component = fixture.componentInstance;
