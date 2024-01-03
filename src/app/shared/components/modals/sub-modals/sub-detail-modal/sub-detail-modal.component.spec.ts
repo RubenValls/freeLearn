ed10f1f8a85dbc5c9d9d79b877db0d4c6c2fe8ba
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-03 14:17:01 +0100

    fix sub detail modal component tests

--- src/app/shared/components/modals/sub-modals/sub-detail-modal/sub-detail-modal.component.spec.ts
+++ src/app/shared/components/modals/sub-modals/sub-detail-modal/sub-detail-modal.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubDetailModalComponent } from './sub-detail-modal.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { StoreModule } from '@ngrx/store';
 
 describe('SubDetailModalComponent', () => {
   let component: SubDetailModalComponent;
   let fixture: ComponentFixture<SubDetailModalComponent>;
+  let mockDialogRef: jasmine.SpyObj<MatDialogRef<any>>;
 
   beforeEach(() => {
+    mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     TestBed.configureTestingModule({
-      declarations: [SubDetailModalComponent]
+      declarations: [SubDetailModalComponent],
+      imports: [
+        SharedModule,
+        MatDialogModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: mockDialogRef }
+      ]
     });
     fixture = TestBed.createComponent(SubDetailModalComponent);
     component = fixture.componentInstance;
