d3cc3717599b99eb7b0baa3636060eb830da939b
amsiceza <ismacerverazamora@gmail.com>
2024-01-03 13:28:27 +0100

    Add button in course card storybook

--- src/app/shared/components/course-card/course-card.components.stories.ts
+++ src/app/shared/components/course-card/course-card.components.stories.ts
@@ -1,11 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 
 import { CourseCardComponent } from './course-card.component';
+import { ButtonComponent } from '../button/button.component';
 
 const meta: Meta<CourseCardComponent> = {
   title: 'Components/CourseCard',
   component: CourseCardComponent,
   tags: ['autodocs'], 
+  decorators: [
+    moduleMetadata({
+      declarations:[ButtonComponent]
+    })
+  ]
 };
 
 export default meta;
@@ -24,6 +30,7 @@ export const Default: Story = {
       instructorId:[]
     },
   },
+  
 };
 
 
