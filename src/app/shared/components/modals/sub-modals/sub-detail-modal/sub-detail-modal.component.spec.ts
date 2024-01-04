f40f76fe026109ce8ab6f7e0241b242c4df6e2de
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-04 16:38:35 +0100

    Improved sub modal components tests

--- src/app/shared/components/modals/sub-modals/sub-detail-modal/sub-detail-modal.component.spec.ts
+++ src/app/shared/components/modals/sub-modals/sub-detail-modal/sub-detail-modal.component.spec.ts
@@ -2,16 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubDetailModalComponent } from './sub-detail-modal.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { StoreModule } from '@ngrx/store';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { Store, StoreModule } from '@ngrx/store';
+import { of } from 'rxjs';
+import { FormBuilder } from '@angular/forms';
 
 describe('SubDetailModalComponent', () => {
   let component: SubDetailModalComponent;
   let fixture: ComponentFixture<SubDetailModalComponent>;
   let mockDialogRef: jasmine.SpyObj<MatDialogRef<any>>;
+  let store: any;
+  let dialog: MatDialog;
 
   beforeEach(() => {
     mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+    store = {
+      select: jasmine.createSpy().and.returnValue(of({ lessons: [] })),
+    };
 
     TestBed.configureTestingModule({
       declarations: [SubDetailModalComponent],
@@ -22,15 +29,58 @@ describe('SubDetailModalComponent', () => {
       ],
       providers: [
         { provide: MAT_DIALOG_DATA, useValue: {} },
-        { provide: MatDialogRef, useValue: mockDialogRef }
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: Store, useValue: store },
+        { provide: MatDialog, useValue: { open: () => ({ afterClosed: () => of(true) }) }}
       ]
     });
     fixture = TestBed.createComponent(SubDetailModalComponent);
     component = fixture.componentInstance;
+    component.data = { editForm: new FormBuilder().group({}) };
+    dialog = TestBed.inject(MatDialog);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('onInit', () => {
+    it('should initialize mainForm and lessons on ngOnInit', () => {
+      component.ngOnInit();
+      expect(component.mainForm).toBeTruthy();
+      expect(component.lessons).toEqual([]);
+    });
+  })
+
+  describe('close', () => {
+    it('should close the dialog', () => {
+      component.close();
+      expect(component.dialogRef.close).toHaveBeenCalled();
+    });
+  });
+
+  describe('onAddLesson', () => {
+    it('should call onAddLesson', () => {
+      spyOn(dialog, 'open').and.callThrough();
+      component.onAddLesson();
+      expect(dialog.open).toHaveBeenCalled();
+    });
+  })
+
+  describe('deleteLesson', () => {
+    it('should call deleteLesson', () => {
+      spyOn(dialog, 'open').and.callThrough();
+      component.deleteLesson({name: '', videoUrl: ''});
+      expect(dialog.open).toHaveBeenCalled();
+    });
+  })
+
+  describe('editLesson', () => {
+    it('should call editLesson', () => {
+      spyOn(dialog, 'open').and.callThrough();
+      component.editLesson({name: '', videoUrl: ''}, 1);
+      expect(dialog.open).toHaveBeenCalled();
+    });
+  })
 });
--- src/app/shared/components/modals/sub-modals/sub-modal-create/sub-modal-create.component.spec.ts
+++ src/app/shared/components/modals/sub-modals/sub-modal-create/sub-modal-create.component.spec.ts
@@ -5,6 +5,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 describe('SubModalCreateComponent', () => {
   let component: SubModalCreateComponent;
@@ -33,5 +34,50 @@ describe('SubModalCreateComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onInit', () => {
+    it('should initialize mainForm and lessons on ngOnInit', () => {
+      component.data = { editForm: new FormBuilder().group({}), lessons: [] };
+      component.ngOnInit();
+      expect(component.mainForm).toBeTruthy();
+      expect(component.lessons).toEqual([]);
+    });
+  })
+
+  describe('onAddLesson', () => {
+    it('should add a lesson when onAddLesson is called', () => {
+      const lessonFormValue = { id: '', name: 'Test Lesson', videoUrl: 'http://test.com' };
+      component.lessons = [];
+      component.mainForm = new FormGroup({
+        lessons: new FormControl([])
+      })
+      component.lessonForm.setValue(lessonFormValue);
+      component.onAddLesson();
+      expect(component.mainForm.get('lessons')?.value[0].name).toEqual(lessonFormValue.name);
+      expect(component.mainForm.get('lessons')?.value[0].videoUrl).toEqual(lessonFormValue.videoUrl);
+    });
+  })
+
+  describe('onEditLesson', () => {
+    it('should edit a lesson when onEditLesson is called', () => {
+      const lessonFormValue = { id: '1', name: 'Test Lesson', videoUrl: 'http://test.com' };
+      const updatedLessonFormValue = { id: '1', name: 'Updated Test Lesson', videoUrl: 'http://updatedtest.com' };
+      component.lessons = [lessonFormValue];
+      component.mainForm = new FormGroup({
+        lessons: new FormControl(component.lessons)
+      });
+      component.data = { index: 0 };
+      component.lessonForm.setValue(updatedLessonFormValue);
+      component.onEditLesson();
+      expect(component.mainForm.get('lessons')?.value[0]).toEqual(updatedLessonFormValue);
+    });
+  });
+
+  describe('close', () => {
+    it('should close the dialog', () => {
+      component.close();
+      expect(component.dialogRef.close).toHaveBeenCalled();
+    });
   });
 });
