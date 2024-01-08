bfb9e9932509eacacac15db50d0a8853c5fbe5f4
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-08 08:59:54 +0100

    refactor dispatch actions to a collection

---
+++ src/app/store/effectsActions.ts
@@ -0,0 +1,6 @@
+export const effectsActions = {
+    fetchCourses: { type: 'Fetch Courses' },
+    fetchInstructors: { type: 'Fetch Instructors' },
+    fetchTechnologies: { type: 'Fetch Technologies' },
+    fetchUsers: { type: 'Fetch Users' }
+}
