dced2d8134a20adf836bfeced2b63ace8cda6e3f
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-08 09:21:05 +0100

    added general fetchers to students dashboard and updated test

--- src/app/students/students-dashboard/students-dashboard.component.spec.ts
+++ src/app/students/students-dashboard/students-dashboard.component.spec.ts
@@ -3,10 +3,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StudentsDashboardComponent } from './students-dashboard.component';
 import { StudentsModule } from '../students.module';
 import { RouterTestingModule } from '@angular/router/testing';
+import { effectsActions } from 'src/app/store/effectsActions';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 describe('StudentsDashboardComponent', () => {
   let component: StudentsDashboardComponent;
   let fixture: ComponentFixture<StudentsDashboardComponent>;
+  let store: MockStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,14 +17,39 @@ describe('StudentsDashboardComponent', () => {
       imports: [
         StudentsModule,
         RouterTestingModule,
+      ],
+      providers: [
+        provideMockStore(),
       ]
     });
     fixture = TestBed.createComponent(StudentsDashboardComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch fetchTechnologies on ngOnInit', () => {
+    const action = effectsActions.fetchTechnologies;
+    const spy = spyOn(store, 'dispatch');
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalledWith(action);
+  });
+
+  it('should dispatch fetchInstructors on ngOnInit', () => {
+    const action = effectsActions.fetchInstructors;
+    const spy = spyOn(store, 'dispatch');
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalledWith(action);
+  });
+
+  it('should dispatch fetchCourses on ngOnInit', () => {
+    const action = effectsActions.fetchCourses;
+    const spy = spyOn(store, 'dispatch');
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalledWith(action);
   });
 });
--- src/app/students/students-dashboard/students-dashboard.component.ts
+++ src/app/students/students-dashboard/students-dashboard.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { DeleteModalComponent } from 'src/app/shared/components/modals/delete-modal/delete-modal.component';
-import { DetailModalComponent } from 'src/app/shared/components/modals/detail-modal/detail-modal.component';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { effectsActions } from 'src/app/store/effectsActions';
 
 @Component({
   selector: 'app-students-dashboard',
   templateUrl: './students-dashboard.component.html',
   styleUrls: ['./students-dashboard.component.scss']
 })
-export class StudentsDashboardComponent {
+export class StudentsDashboardComponent implements OnInit{
 
-  
+  constructor(
+    private store: Store,
+  ) { } 
+
+  ngOnInit(): void {
+    this.store.dispatch(effectsActions.fetchTechnologies);
+    this.store.dispatch(effectsActions.fetchInstructors);
+    this.store.dispatch(effectsActions.fetchCourses);
+  }
 }
