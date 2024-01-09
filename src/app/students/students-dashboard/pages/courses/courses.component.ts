f7608a441640a254d52057ec44db90b39a6721a9
Mayra <mayra28limo@gmail.com>
2024-01-09 10:40:23 +0100

    Refactor course cards layout and add dynamic course data

--- src/app/students/students-dashboard/pages/courses/courses.component.ts
+++ src/app/students/students-dashboard/pages/courses/courses.component.ts
@@ -7,20 +7,7 @@ import { selectCourses } from 'src/app/store/courses/courses.selectors';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent implements OnInit {
-  courses$ = this.store.select(selectCourses);
-  courses: any;
-  
-    constructor(
-      private store: Store,
-    
-    ) {}
-    ngOnInit(): void {
-      this.courses$.subscribe((courses) => {
-        this.courses = courses;
-      });
-    }
-
-     
+export class CoursesComponent {
+ 
 
 }
