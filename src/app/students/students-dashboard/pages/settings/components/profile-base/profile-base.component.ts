1ee60384bd6dfe7c9c3e1135c34bb6a2a246cda5
amsiceza <ismacerverazamora@gmail.com>
2024-01-09 11:30:25 +0100

    Create scrollbar students settings

--- src/app/students/students-dashboard/pages/settings/components/profile-base/profile-base.component.ts
+++ src/app/students/students-dashboard/pages/settings/components/profile-base/profile-base.component.ts
@@ -1,30 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { selectUser } from 'src/app/login/store/user.selectors';
+import { UpdateModalComponent } from 'src/app/shared/components/modals/update-modal/update-modal.component';
 
 @Component({
   selector: 'app-profile-base',
   templateUrl: './profile-base.component.html',
-  styleUrls: ['./profile-base.component.scss']
+  styleUrls: ['./profile-base.component.scss'],
 })
 export class ProfileBaseComponent implements OnInit {
   user$ = this.store.select(selectUser);
-  profileForm: FormGroup;  
+  profileForm: FormGroup;
 
-  constructor(private store: Store, private fb: FormBuilder) {
+  constructor(
+    private store: Store,
+    private fb: FormBuilder,
+    public dialog: MatDialog
+  ) {
     this.profileForm = this.fb.group({
       email: ['', Validators.required],
       phoneNumber: [''],
       displayName: [''],
-      photoURL: ['']
+      photoURL: [''],
     });
   }
 
   ngOnInit() {
-    this.user$.subscribe(user => {
+    this.user$.subscribe((user) => {
       if (user) {
-        this.profileForm.patchValue(user);  
+        this.profileForm.patchValue(user);
       }
     });
   }
@@ -32,7 +38,9 @@ export class ProfileBaseComponent implements OnInit {
   onSubmit() {
     if (this.profileForm.valid) {
       console.log(this.profileForm.value);
+      this.dialog.open(UpdateModalComponent, {data:{
+        data: this.profileForm.value
+      }})
     }
   }
 }
-
