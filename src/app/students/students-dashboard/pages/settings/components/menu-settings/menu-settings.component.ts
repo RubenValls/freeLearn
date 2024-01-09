0281141fcfae4dbb7b0c3458a204bdb02325fef2
amsiceza <ismacerverazamora@gmail.com>
2024-01-09 17:19:47 +0100

    Log out works, without alert service

--- src/app/students/students-dashboard/pages/settings/components/menu-settings/menu-settings.component.ts
+++ src/app/students/students-dashboard/pages/settings/components/menu-settings/menu-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-settings',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-settings.component.scss']
 })
 export class MenuSettingsComponent {
+
+  constructor(private router: Router){
+
+  }
+
+  handleLogOut(){
+    localStorage.removeItem('userInfo');
+    sessionStorage.removeItem('userInfo')
+    this.router.navigate(['/login'])
+  }
+
 
 }
