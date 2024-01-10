9d66993b89b0f8b52c1a5b286badf941731d51fb
amsiceza <ismacerverazamora@gmail.com>
2024-01-10 13:10:18 +0100

    Add techs with store in student tech

--- src/app/students/students-dashboard/pages/technologies/technologies.component.ts
+++ src/app/students/students-dashboard/pages/technologies/technologies.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { TechnologyType } from 'src/app/admins/admins-dashboard/pages/technologies/types/technologies';
+import { selectTechnologies } from 'src/app/store/technologies/tecnologies.selectors';
 
 @Component({
   selector: 'app-technologies',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./technologies.component.scss']
 })
 export class TechnologiesComponent {
+
+  tech$ = this.store.select(selectTechnologies);
+  techs: any
+
+  constructor(private store: Store){
+
+  }
+
+  ngOnInit() {
+    this.tech$.subscribe((tech) => {
+      this.techs = tech; 
+    });
+  }
 
 }
