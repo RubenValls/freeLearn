2de90ee2bde4ccdb3bd3c940c77fa4836bf7a605
amsiceza <ismacerverazamora@gmail.com>
2024-01-10 11:46:19 +0100

    Update password works well

--- src/app/students/students-dashboard/pages/settings/components/password/password.component.ts
+++ src/app/students/students-dashboard/pages/settings/components/password/password.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { getAuth, updatePassword } from 'firebase/auth';
+import { AlertsService } from 'src/app/shared/services/alerts/alerts.service';
 
 @Component({
   selector: 'app-password',
@@ -13,34 +14,49 @@ export class PasswordComponent {
   hidePassword = true;
   hidePassword2 = true;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private alertMessage: AlertsService
+  ) {
     this.resetPassForm = this.fb.group({
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required],
-    })
+    });
   }
 
   cambiarContrasena() {
-    const auth = getAuth();
-    const user = auth.currentUser;
-
-    if (user) {
+    if (this.resetPassForm.valid) {
+      const auth = getAuth();
+      const user = auth.currentUser;
       const newPassword = this.resetPassForm.value.password;
+      const confirmPassword = this.resetPassForm.value.confirmPassword;
 
-      updatePassword(user, newPassword)
-        .then(() => {
-          console.log('Contraseña actualizada exitosamente.');
-          this.router.navigate(['/students/settings/profile']);
-        })
-        .catch((error) => {
-          console.error('Error al cambiar la contraseña:', error.message);
-        });
-    } else {
-      console.error('No hay usuario autenticado.');
+      if (user) {
+        if (newPassword == confirmPassword){
+
+        
+          updatePassword(user, newPassword)
+            .then(() => {
+              this.alertMessage.successMessage('Password changed succesfully');
+              this.router.navigate(['/students/settings/profile']);
+            })
+            .catch((error) => {
+              if (
+                error.message == 'Firebase: Error (auth/requires-recent-login).'
+              ) {
+                this.alertMessage.errorMessage(
+                  'You have to log in again to change password'
+                );
+              }
+              console.error('Error change password:', error.message);
+            });
+          }else{
+            this.alertMessage.errorMessage("Passwords no match")
+          }
+      } else {
+        console.error('No auth user.');
+      }
     }
   }
-
-
-  
 }
-
