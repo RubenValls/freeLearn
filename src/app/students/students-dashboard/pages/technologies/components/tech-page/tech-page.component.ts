a93bc8ac7ee3709b376e0b3ef2fff0a7cd62d65b
amsiceza <ismacerverazamora@gmail.com>
2024-01-10 15:43:40 +0100

    Update date tech

--- src/app/students/students-dashboard/pages/technologies/components/tech-page/tech-page.component.ts
+++ src/app/students/students-dashboard/pages/technologies/components/tech-page/tech-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { TechService } from 'src/app/admins/admins-dashboard/pages/technologies/service/tech.service';
+import { TechnologyType } from 'src/app/admins/admins-dashboard/pages/technologies/types/technologies';
 
 
 @Component({
@@ -7,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./tech-page.component.scss']
 })
 export class TechPageComponent {
+
+  tech: TechnologyType | undefined;
   
+  techId: string = '';
+  techSubscription: Subscription | undefined;
+  techIdSubscription: Subscription | undefined;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private techService: TechService,
+    ) {
+    this.techSubscription = this.route.data.subscribe(data => {
+      this.tech = data['data']
+    });
+    this.techIdSubscription = this.route.paramMap.subscribe((params) => {
+      const idParam = params.get('id');
+      this.techId = idParam ? idParam : ''
+    })
+  }
 }
