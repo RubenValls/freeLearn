7d1ee03ff6b315a75db97329978480eb7480a79c
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-10 15:57:49 +0100

    fix header component at instructor detail page

--- src/app/students/students-dashboard/pages/trainers/components/instructor-page/components/header/header.component.spec.ts
+++ src/app/students/students-dashboard/pages/trainers/components/instructor-page/components/header/header.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { StudentsModule } from 'src/app/students/students.module';
 
-describe('HeaderComponent', () => {
+fdescribe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
+      imports: [
+        StudentsModule
+      ]
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -17,5 +21,12 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit update event when rating changes', () => {
+    spyOn(component.handleUpdate, 'emit');
+    const newRating = 5;
+    component.onRatingChanged(newRating);
+    expect(component.handleUpdate.emit).toHaveBeenCalledWith(newRating);
   });
 });
