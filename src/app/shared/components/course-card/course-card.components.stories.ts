5a8675662a8a95d5fb70252765c4c5ca39a073aa
amsiceza <ismacerverazamora@gmail.com>
2024-01-11 16:27:13 +0100

    restructuring folder organization storybook

--- src/app/shared/components/course-card/course-card.components.stories.ts
+++ src/app/shared/components/course-card/course-card.components.stories.ts
@@ -4,7 +4,7 @@ import { CourseCardComponent } from './course-card.component';
 import { ButtonComponent } from '../button/button.component';
 
 const meta: Meta<CourseCardComponent> = {
-  title: 'Components/CourseCard',
+  title: 'Components/Shared/CourseCard',
   component: CourseCardComponent,
   tags: ['autodocs'], 
   decorators: [
