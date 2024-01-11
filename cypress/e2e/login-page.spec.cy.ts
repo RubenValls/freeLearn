91039b8d8f2afcd3c9533a6dd6a25cd5dd6a1b9d
amsiceza <ismacerverazamora@gmail.com>
2024-01-11 12:46:07 +0100

    Charge login - login with admin

--- cypress/e2e/login-page.spec.cy.ts
+++ cypress/e2e/login-page.spec.cy.ts
@@ -1,9 +1,22 @@
 describe('Login Page', () => {
-  it('visit login page', () => {
+
+  it('charge login page', () => {
+    cy.visit('/login')
+   
+  })
+
+  it('login with student', () => {
     cy.visit('/login')
     cy.get('input[formControlName="email"]').type('ruben@test.com')
     cy.get('input[formControlName="password"]').type('test1234')
     cy.get('#loginButton').click()
     cy.url().should('include', '/students')
+  })
+  it('login with admin', () => {
+    cy.visit('/login')
+    cy.get('input[formControlName="email"]').type('admin@freelearn.com')
+    cy.get('input[formControlName="password"]').type('admin2023')
+    cy.get('#loginButton').click()
+    cy.url().should('include', '/admin')
   })
 })
