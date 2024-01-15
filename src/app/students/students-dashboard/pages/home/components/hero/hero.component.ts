7714b01ad399370704e7295ccd5f167e5763efa5
amsiceza <ismacerverazamora@gmail.com>
2024-01-15 11:08:54 +0100

    Get user from session and local for hero

--- src/app/students/students-dashboard/pages/home/components/hero/hero.component.ts
+++ src/app/students/students-dashboard/pages/home/components/hero/hero.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+  user: any;
 
+  ngOnInit(): void {
+    this.getUserLogged()
+    console.log(this.user);
+  }
+
+  getUserLogged() {
+    const localUser = localStorage.getItem('userInfo');
+    const sessionUser = sessionStorage.getItem('userInfo');
+  
+    if (localUser) {
+      this.user = JSON.parse(localUser); // Convierte la cadena a objeto
+    } else if (sessionUser) {
+      this.user = JSON.parse(sessionUser); // Convierte la cadena a objeto
+    }
+  }
+  
 }
