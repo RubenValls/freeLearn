305f165742b3453cdcbd6cac2497c4af8f3a7185
amsiceza <ismacerverazamora@gmail.com>
2024-01-16 12:00:43 +0100

    HomeCard for tech in storybook

--- src/app/students/students-dashboard/pages/home/components/custom-card/custom-card.components.stories.ts
+++ src/app/students/students-dashboard/pages/home/components/custom-card/custom-card.components.stories.ts
@@ -1,44 +1,62 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { CustomCardComponent } from './custom-card.component';
 
-
-
 //👇 This default export determines where your story goes in the story list
 const meta: Meta<CustomCardComponent> = {
-    title: "Components/Students/HomeCard",
-    component: CustomCardComponent,
-    tags: ['autodocs'],
+  title: 'Components/Students/HomeCard',
+  component: CustomCardComponent,
+  tags: ['autodocs'],
 };
 
 export default meta;
 type Story = StoryObj<CustomCardComponent>;
 
-export const Course: Story = {
+export const Courses: Story = {
   args: {
-    data:{
-        name: "React",
-        imageUrl: "https://miro.medium.com/v2/resize:fit:1200/1*XOSA6j_mJ3oz6g8MfoHDpg.jpeg",
-        description: "React",
-        lessons: [],
-        techs: [{id:"1", name: "React"}],
-        rating: [],
-        introductionURL: "https://example.com/introduction",
-        instructorId: [
-            {id:"1", name: "Midudev"}
-        ]
-    }
+    data: {
+      name: 'React',
+      imageUrl:
+        'https://miro.medium.com/v2/resize:fit:1200/1*XOSA6j_mJ3oz6g8MfoHDpg.jpeg',
+      description: 'React',
+      lessons: [],
+      techs: [{ id: '1', name: 'React' }],
+      rating: [],
+      introductionURL: 'https://example.com/introduction',
+      instructorId: [{ id: '1', name: 'Midudev' }],
+    },
   },
 };
 
-export const Tech: Story = {
-    args: {
-        data: {
-            id: '1',
-            name: 'Python',
-            description: 'Python',
-            imagePath:
-              'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
-            courses: [],
-          },
+export const Techs: Story = {
+  args: {
+    data: {
+      id: '1',
+      name: 'Python',
+      description: 'Python',
+      imagePath:
+        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+      courses: [],
     },
-  };
+  },
+};
+
+export const Trainers: Story = {
+  args: {
+    data: {
+      id: '1',
+      name: 'Midudev',
+      reating: [],
+      imagePath:
+        'https://yt3.googleusercontent.com/ytc/AIf8zZScNwk4VuB2XijvUaEz7Z8Wff_tvBzYaHgOXZZtFQ=s176-c-k-c0x00ffffff-no-rj',
+      courses: [],
+      socialMedia: [
+        {
+          linkedin: 'https://www.linkedin.com/in/midudev',
+          twitter: 'https://twitter.com/midudev',
+          web: 'https://www.youtube.com/c/midudev',
+          youtube: 'https://www.youtube.com/c/midudev',
+        },
+      ],
+    },
+  },
+};
