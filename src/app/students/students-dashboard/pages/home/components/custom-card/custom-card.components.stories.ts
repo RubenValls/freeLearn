4a2a90694c8add493b8d2528a65590f053a9d743
amsiceza <ismacerverazamora@gmail.com>
2024-01-16 11:40:50 +0100

    HomeCard for course in storybook

---
+++ src/app/students/students-dashboard/pages/home/components/custom-card/custom-card.components.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { CustomCardComponent } from './custom-card.component';
+
+
+
+//👇 This default export determines where your story goes in the story list
+const meta: Meta<CustomCardComponent> = {
+    title: "Components/Students/HomeCard",
+    component: CustomCardComponent,
+    tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<CustomCardComponent>;
+
+export const Course: Story = {
+  args: {
+    data:{
+        name: "React",
+        imageUrl: "https://miro.medium.com/v2/resize:fit:1200/1*XOSA6j_mJ3oz6g8MfoHDpg.jpeg",
+        description: "React",
+        lessons: [],
+        techs: [{id:"1", name: "React"}],
+        rating: [],
+        introductionURL: "https://example.com/introduction",
+        instructorId: [
+            {id:"1", name: "Midudev"}
+        ]
+    }
+  },
+};
+
+export const Tech: Story = {
+    args: {
+      data:{
+          name: "React",
+          imageUrl: "https://miro.medium.com/v2/resize:fit:1200/1*XOSA6j_mJ3oz6g8MfoHDpg.jpeg",
+          description: "React",
+          lessons: [],
+          techs: [{id:"1", name: "React"}],
+          rating: [],
+          introductionURL: "https://example.com/introduction",
+          instructorId: [
+              {id:"1", name: "Midudev"}
+          ]
+      }
+    },
+  };
