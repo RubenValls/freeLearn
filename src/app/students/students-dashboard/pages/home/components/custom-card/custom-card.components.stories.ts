ca152b20e76d78e27d4af40168b27da3eb094c55
amsiceza <ismacerverazamora@gmail.com>
2024-01-16 11:42:32 +0100

    HomeCard for tech in storybook

--- src/app/students/students-dashboard/pages/home/components/custom-card/custom-card.components.stories.ts
+++ src/app/students/students-dashboard/pages/home/components/custom-card/custom-card.components.stories.ts
@@ -32,17 +32,13 @@ export const Course: Story = {
 
 export const Tech: Story = {
     args: {
-      data:{
-          name: "React",
-          imageUrl: "https://miro.medium.com/v2/resize:fit:1200/1*XOSA6j_mJ3oz6g8MfoHDpg.jpeg",
-          description: "React",
-          lessons: [],
-          techs: [{id:"1", name: "React"}],
-          rating: [],
-          introductionURL: "https://example.com/introduction",
-          instructorId: [
-              {id:"1", name: "Midudev"}
-          ]
-      }
+        data: {
+            id: '1',
+            name: 'Python',
+            description: 'Python',
+            imagePath:
+              'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+            courses: [],
+          },
     },
   };
