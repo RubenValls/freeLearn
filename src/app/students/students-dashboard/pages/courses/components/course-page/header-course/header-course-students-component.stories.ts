c4e3182f3d7d3b78c91468906ce33cc0b9aa74ea
Mayra <mayra28limo@gmail.com>
2024-01-17 16:09:48 +0100

    fix header components

--- src/app/students/students-dashboard/pages/courses/components/course-page/header-course/header-course-students-component.stories.ts
+++ src/app/students/students-dashboard/pages/courses/components/course-page/header-course/header-course-students-component.stories.ts
@@ -3,34 +3,44 @@ import { HeaderCourseComponent } from "./header-course.component";
 import { ActivatedRoute } from "@angular/router";
 import { of } from "rxjs";
 import { UsersService } from "src/app/shared/services/users/users.service";
+import { AlertsService } from "src/app/shared/services/alerts/alerts.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { Firestore, FirestoreModule } from "@angular/fire/firestore";
+import { FirebaseApp, FirebaseAppModule } from "@angular/fire/app";
+import { StoreModule } from "@ngrx/store";
+import { userReducer } from "src/app/login/store/user.reducer";
 
-const meta: Meta<HeaderCourseComponent> = {
+export default {
     title: "Components/Students/HeaderCourse",
     component: HeaderCourseComponent,
     tags: ['autodocs'],
     decorators: [
         moduleMetadata({
-            providers: [
-                {
-                    provide: ActivatedRoute,
-                    useValue: {
-                        params: of({ id: 'test' }),
-                        data: of({ data: { id: 'test' } })
-                    }
-                },
-                { provide: UsersService, useValue: { getUserFromStorage: () => ({ id: 'test' }) } },
+            imports: [BrowserAnimationsModule, MatSnackBarModule, FirestoreModule, FirebaseAppModule, StoreModule.forRoot({ user: userReducer }),],
+            providers: [AlertsService, UsersService, {
+                provide: ActivatedRoute,
+                useValue: {
+                    params: of({ id: "1"}),
+                    data: of({ })
+                }
+            },
+                { provide: Firestore, useValue: {} },
+                { provide: FirebaseApp, useValue: {} },
+             
+                
 
-            ],
-
+            ]
         })
     ]
+}
 
-};
-export default meta;
-type Story = StoryObj<HeaderCourseComponent>;
-export const Default: Story = {
-    args: {
+
+export const Default = () => ({
+    component: HeaderCourseComponent,
+    props: {
         course: {
+           
             name: "Curso Completo de REACT",
             imageUrl: "https://www.pildorasinformaticas.es/wp-content/uploads/2021/01/AngularJS-Tutorial.png",
             description: "Comienzo en el día de hoy un curso de Angular que me habéis solicitado mucho desde los comentarios del canal, mi web y redes sociales. En este primer vídeo vemos el temario del curso, los conocimientos previos requeridos y vemos qué es Angular y sus principales características. Espero que el curso sea de vuestro agrado.Para más cursos, ejercicios y manuales visita: www.pildorasinformaticas.es",
@@ -41,6 +51,9 @@ export const Default: Story = {
             instructorId: [
                 { id: "1", name: "JAB" }
             ]
-        }
+        },
+        isFavorite: false,
+        courseId: "ggggygy"
+
     }
-};
+});
