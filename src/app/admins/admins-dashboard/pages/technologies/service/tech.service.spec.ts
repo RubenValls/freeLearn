c322b946b98ad4fd35619421f3e094e3f16e6c14
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-19 08:05:13 +0100

    add add technologies tests

--- src/app/admins/admins-dashboard/pages/technologies/service/tech.service.spec.ts
+++ src/app/admins/admins-dashboard/pages/technologies/service/tech.service.spec.ts
@@ -5,7 +5,8 @@ import { AdminsModule } from 'src/app/admins/admins.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { Firestore, getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { TechnologyType } from '../types/technologies';
 
 describe('TechService', () => {
   let service: TechService;
@@ -25,4 +26,21 @@ describe('TechService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add a technology', async () => {
+    const technology: TechnologyType = {
+      id: '1',
+      name: 'Angular',
+      imagePath: 'path/to/image',
+      description: 'A platform for building web applications.',
+      courses: []
+    };
+    spyOn(service, 'addTechnology').and.callThrough();
+    
+    const newTech = await service.addTechnology(technology);
+    
+    expect(service.addTechnology).toHaveBeenCalledWith(technology);
+    expect(newTech).toBeTruthy();
+  });
+
 });
