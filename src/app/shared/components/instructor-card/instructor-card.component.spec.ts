e7a689594de6b720cfe1bf34675edd831bc23513
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-19 10:19:45 +0100

    add instructor card tests

--- src/app/shared/components/instructor-card/instructor-card.component.spec.ts
+++ src/app/shared/components/instructor-card/instructor-card.component.spec.ts
@@ -30,6 +30,26 @@ describe('InstructorCardComponent', () => {
     expect(component.instructor).toBeUndefined();
   });
 
+  it('should have instructor if input', () => {
+    component.instructor = {
+      id: '1',
+      name: 'John Doe',
+      socialMedia: {
+        web: 'www.johndoe.com',
+        youtube: 'www.youtube.com/johndoe',
+        twitter: 'www.twitter.com/johndoe',
+        linkedin: 'www.linkedin.com/in/johndoe',
+      },
+      courses: ['Course 1', 'Course 2'],
+      imagePath: 'path/to/image',
+      rating: [
+        { userId: 'user1', rating: 5 },
+        { userId: 'user2', rating: 4 },
+      ],
+    }
+    expect(component.instructor).toBeDefined();
+  });
+
   it('should accept instructor input', () => {
     const instructor: Instructor = {
       id: '1',
