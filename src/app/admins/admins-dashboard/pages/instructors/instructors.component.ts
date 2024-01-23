821c228d0df1e0581a9a1a7d81591c24ddc41938
amsiceza <ismacerverazamora@gmail.com>
2024-01-23 16:05:23 +0100

    Add filtred to instructors admins

--- src/app/admins/admins-dashboard/pages/instructors/instructors.component.ts
+++ src/app/admins/admins-dashboard/pages/instructors/instructors.component.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectInstructor } from 'src/app/store/instructors/instructors.selectors';
 import { InstructorsService } from './instructors-service/instructors.service';
 import { AlertsService } from 'src/app/shared/services/alerts/alerts.service';
 import { Instructor } from './instructors';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-instructors',
   templateUrl: './instructors.component.html',
   styleUrls: ['./instructors.component.scss']
 })
-export class InstructorsComponent {
+export class InstructorsComponent implements OnInit{
   showForm: boolean = false;
   instructors$ = this.store.select(selectInstructor);
   modalWith: string = '1034';
   modalHeight: string = '600px';
   modalTitle: string = 'Trainer';
+  filteredInstructor: any = [];
+
 
   tableColumns = [
     { prop: 'name', title: 'Name' },
@@ -42,12 +45,45 @@ export class InstructorsComponent {
     },
   ]
 
+  searchInstructorControl = new FormControl('');
 
   constructor(
     private store: Store,
     private instructorsService: InstructorsService,
     private alertMessages: AlertsService
   ) {
+  }
+
+  ngOnInit(): void {
+    this.instructors$.subscribe((courses) => {
+      this.filteredInstructor = this.filterInstructor(
+        courses,
+        this.searchInstructorControl.value || ''
+      );
+    });
+    this.searchInstructorControl.valueChanges.subscribe((input) => {
+      this.instructors$.subscribe((courses) => {
+        this.filteredInstructor = this.filterInstructor(courses, input || '');
+      });
+    });
+  }
+
+  filterInstructor(array: readonly any[], input: string) {
+    return array.filter(
+      (item) =>
+        item.name.toLowerCase().includes(input.toLowerCase())
+        
+    );
+  }
+
+  getInstructors() {
+    if (this.filteredInstructor.length > 0) {
+      console.log(this.filteredInstructor);
+      return this.filteredInstructor;
+    } else {
+      return this.instructors$;
+    }
+    
   }
 
   toggleForm() {
